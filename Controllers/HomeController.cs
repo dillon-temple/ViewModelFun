d3393e8e672ef2c65885133e290736c978dd017f
Dillon Temple <momo@Dillons-MacBook-Pro.local>
2019-08-12 12:39:40 -0700

    Done

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ViewModelFun.Models;
+
+namespace ViewModelFun.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            string message = "YO BROS WELCOME TO THE INTERNETYO BROS WELCOME TO THE INTERNETYO BROS WELCOME TO THE INTERNETYO BROS WELCOME TO THE INTERNETYO BROS WELCOME TO THE INTERNET";
+
+            return View("Index", message);
+        }
+        [HttpGet("Numbers")]
+        public IActionResult Numbers()
+        {
+
+            int[] numarray = new int[]
+            {
+                1,5,18,21,5636,12,4,7
+            };
+
+            return View("Numbers", numarray);
+        }
+        [HttpGet("User")]
+        public IActionResult User()
+        {
+            User sampleUser = new User("Bobby", "Hill");
+
+            return View(sampleUser);
+        }
+        [HttpGet("Users")]
+        public IActionResult Users(){
+            
+            List<User> viewModel = new List<User>()
+            {
+               new User("Bobby", "Hill"),
+               new User("Hank", "Hill"),
+               new User("Dora", "Explora"),
+               new User("Tony", "Hawk"),
+               new User("Fred", "Durst"),
+            };
+            return View(viewModel);
+        }
+
+        
+
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,10 @@
+public class User
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+
+    public User(string fname, string lname){
+        FirstName = fname;
+        LastName = lname;
+    }
+}
